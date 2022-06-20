b9e41c92b5158fed09097551633fb2fca9ee27b3
Abhi Kumar <58290134+abhistark007@users.noreply.github.com>
2022-06-20 12:58:30 +0530

    created login+register screen with backend

--- firebasexnotes/lib/Views/Auth Screens/login_screen.dart
+++ firebasexnotes/lib/Views/Auth Screens/login_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:firebasexnotes/Controllers/auth_controller.dart';
 import 'package:firebasexnotes/Models/custom_button.dart';
 import 'package:firebasexnotes/Models/custom_text_field.dart';
 import 'package:firebasexnotes/Views/Auth%20Screens/register_screen.dart';
@@ -25,36 +26,46 @@ class _LoginScreenState extends State<LoginScreen> {
     super.dispose();
   }
 
+  final AuthController authController=Get.put(AuthController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: SizedBox(
-        height: double.maxFinite,
-        width: double.maxFinite,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomTextField(labelText: "Email", controller: emailController),
-              SizedBox(height: 30,),
-              CustomTextField(labelText: "Password", controller: passwordController,obscureText: true,),
-              SizedBox(height: 50,),
-              CustomButton(buttonContent: "Login"),
-              SizedBox(height: 30,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Text("Not a member yet ? ",style: TextStyle(color: Colors.black,fontSize: 15),),
-                InkWell(
-                  onTap: (){
-                    Get.to(()=>RegisterScreen());
-                  },
-                  child: Text("Register",style: TextStyle(color: Colors.purple,fontSize: 15,fontWeight: FontWeight.bold),),),
-              ],),
-            ],
+      body: Obx(
+        ()=> SizedBox(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextField(labelText: "Email", controller: emailController),
+                SizedBox(height: 30,),
+                CustomTextField(labelText: "Password", controller: passwordController,obscureText: true,),
+                SizedBox(height: 50,),
+                authController.isLoginLoading.value?Center(child: CircularProgressIndicator())
+                :CustomButton(buttonContent: "Login",onpressed: ()async{
+                  authController.isLoginLoading.value=true;
+                  authController.loginUser(emailController.text, passwordController.text);
+                  authController.isLoginLoading.value=false;
+                },)
+,
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Text("Not a member yet ? ",style: TextStyle(color: Colors.black,fontSize: 15),),
+                  InkWell(
+                    onTap: (){
+                      Get.to(()=>RegisterScreen());
+                    },
+                    child: Text("Register",style: TextStyle(color: Colors.purple,fontSize: 15,fontWeight: FontWeight.bold),),),
+                ],),
+              ],
+            ),
           ),
         ),
       ),
--- firebasexnotes/lib/Views/Auth Screens/register_screen.dart
+++ firebasexnotes/lib/Views/Auth Screens/register_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebasexnotes/Controllers/auth_controller.dart';
 import 'package:firebasexnotes/Models/custom_button.dart';
 import 'package:firebasexnotes/Models/custom_text_field.dart';
 import 'package:firebasexnotes/Views/Auth%20Screens/login_screen.dart';
@@ -24,37 +25,47 @@ class _RegisterScreenState extends State<RegisterScreen> {
     passwordController.dispose();
     super.dispose();
   }
+
+  final AuthController authController=Get.find();
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: SizedBox(
-        height: double.maxFinite,
-        width: double.maxFinite,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomTextField(labelText: "Email", controller: emailController),
-              SizedBox(height: 30,),
-              CustomTextField(labelText: "Password", controller: passwordController,obscureText: true,),
-              SizedBox(height: 50,),
-              CustomButton(buttonContent: "Register"),
-              SizedBox(height: 30,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                Text("Already a member ? ",style: TextStyle(color: Colors.black,fontSize: 15),),
-                InkWell(
-                  onTap: (){
-                    Get.to(()=>LoginScreen());
-                  },
-                  child: Text("Login",style: TextStyle(color: Colors.purple,fontSize: 15,fontWeight: FontWeight.bold),),),
-              ],),
-            ],
+      body: Obx(
+        ()=> SizedBox(
+          height: double.maxFinite,
+          width: double.maxFinite,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextField(labelText: "Email", controller: emailController),
+                SizedBox(height: 30,),
+                CustomTextField(labelText: "Password", controller: passwordController,obscureText: true,),
+                SizedBox(height: 50,),
+                authController.isRegisterLoading.value?
+                Center(child: CircularProgressIndicator(),):
+                CustomButton(buttonContent: "Register",onpressed: ()async{
+                  authController.isRegisterLoading.value=true;
+                  authController.registerUser(emailController.text, passwordController.text);
+                  authController.isRegisterLoading.value=true;
+                },),
+                SizedBox(height: 30,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Text("Already a member ? ",style: TextStyle(color: Colors.black,fontSize: 15),),
+                  InkWell(
+                    onTap: (){
+                      Get.to(()=>LoginScreen());
+                    },
+                    child: Text("Login",style: TextStyle(color: Colors.purple,fontSize: 15,fontWeight: FontWeight.bold),),),
+                ],),
+              ],
+            ),
           ),
         ),
       ),
