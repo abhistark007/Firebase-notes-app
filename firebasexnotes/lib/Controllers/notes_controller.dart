4733dd34fb001645a9716aaa4f56628405c00a03
Abhi Kumar <58290134+abhistark007@users.noreply.github.com>
2022-06-20 16:57:46 +0530

    Update notes_controller.dart

--- firebasexnotes/lib/Controllers/notes_controller.dart
+++ firebasexnotes/lib/Controllers/notes_controller.dart
@@ -1,22 +1,36 @@
+// ignore_for_file: avoid_print
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebasexnotes/Controllers/auth_controller.dart';
+import 'package:firebasexnotes/Models/notes_model.dart';
 import 'package:get/get.dart';
 final AuthController authController=Get.find();
 class NotesController extends GetxController{
   FirebaseFirestore firebaseFirestore=FirebaseFirestore.instance;
   Rx<bool> addNotesLoading=false.obs;
+  Rx<bool> displayNotesoading=false.obs;
+  List<Notes> notesList=[];
   // Add Notes
-  Future addNotes(String txt)async{
+  Future addNotes(String txt,Notes notes)async{
     try{
     //   await firebaseFirestore.collection("notes").doc().set({
     //   "task":txt
     // });
-    await firebaseFirestore.collection("notes").doc(authController.firebaseAuth.currentUser!.uid).collection(authController.firebaseAuth.currentUser!.email!).doc().set({
-      "goals":txt
-    });
+    await firebaseFirestore.collection("notes").doc(authController.firebaseAuth.currentUser!.uid).collection(authController.firebaseAuth.currentUser!.email!).doc().set(notes.toMap(notes));
     Get.snackbar("Successfully", "Notes Added");
     }catch(e){
       Get.snackbar("Error Adding Notes", e.toString());
     }
+  }
+
+  // Display Notes
+  Future getNotesList()async{
+    QuerySnapshot querySnapshot= await firebaseFirestore.collection("notes").doc(authController.firebaseAuth.currentUser!.uid).collection(authController.firebaseAuth.currentUser!.email!).get();
+    //notesList=querySnapshot.docs;
+    for(var i in querySnapshot.docs){
+      notesList.add(Notes(txt: i["goals"]));
+      print(i["goals"]);
+    }
+    return notesList;
   }
 }
