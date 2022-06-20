43f3d6830cbc5f65c145b8edda5f0f02489abce4
Abhi Kumar <58290134+abhistark007@users.noreply.github.com>
2022-06-20 12:23:23 +0530

    Update main.dart

--- firebasexnotes/lib/main.dart
+++ firebasexnotes/lib/main.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebasexnotes/Views/Auth%20Screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
